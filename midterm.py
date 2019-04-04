90c7fea8ee72c8c57e97dbd2a5f9aa864436cb3a
Brajam <47007305+Brajam@users.noreply.github.com>
2019-04-04 08:21:48 -0500

    Update midterm.py
    Added first function on the main script (top descritpions) and several inputs with defaults to avoid execution errors.

--- midterm.py
+++ midterm.py
@@ -22,36 +22,32 @@ from flask import request
 
 # First function, to get the files from the web
 def getfile(Input:"String list or url with the file"):
-    file_head = head(Input)
+    #file_head = head(Input)
     file_get  = get(Input,stream = True)
-    size = file_get.headers
+    #size = file_get.headers
     ## working on doing  a validation to avoid retrieving a file too big
     #if size < 100:
-    print(size)
-    print(file_head.headers)
+    #print(size)
+    #print(file_head.headers)
     text = file_get.text
     return(text)
 
 ## I used this function on one of the prior assigments, it gaves the top 10 descriptors, so we can keep it and
 ## adjusted as needed
-def topdescriptions(File_name:"Name of the file with the characters"):
+def topdescriptions(File_name:"Name of the file with the characters",Top_N:"Number of Top descriptors to be retrieved"):
     # Function to identify most common descriptors
     # As input we'll include the list of characters cleaned
-    with open(File_name,'r') as whole_file:
-        # Reading the whole file, as TexBlob will be applied to the whole
-        whole_text = whole_file.read()
-        tb_text = TextBlob(whole_text)
-        adj_list =[]
-        # Once the TextBlob is created, a loop is performed over the list of words
-        # to identify the descriptors (PoS like JJ are adjetives)
-        for word,pos in tb_text.tags:
-            if pos[0:2] =="JJ":
-                adj_list.append(word)
-        # Creating a Dataframe indicating how many times the adjetive is on the list
-        count_dataframe = pd.Series(adj_list).value_counts()
-        # Printing top ten
-        print("The 10 most common descriptions for characters:")
-        print(count_dataframe.head(n=10))
+    tb_text = TextBlob(File_name)
+    adj_list =[]
+    # Once the TextBlob is created, a loop is performed over the list of words
+    # to identify the descriptors (PoS like JJ are adjetives)
+    for word,pos in tb_text.tags:
+        if pos[0:2] =="JJ":
+            adj_list.append(word)
+    # Creating a Dataframe indicating how many times the adjetive is on the list
+    count_dataframe = pd.Series(adj_list).value_counts()
+    result_desc = count_dataframe
+    return(result_desc)
 
 
 
@@ -62,11 +58,33 @@ app = Flask(__name__)
 
 @app.route('/midterm', methods=['GET'])
 
-def topdescriptions():
-    page = request.args.get('url', default='https://sit.instructure.com/files/4653201/download?download_frd=1&verifier=aWl6ZvPwzQ6wPLZ8H6yTh7O1lJ4Dq9ryBWReSW3M', type=str)
-    a = getfile(page)
-    print(a)
-    #topdescriptions('cleanedlist.txt')
+
+
+def main_process():
+    # defaults
+    address_default = 'https://sit.instructure.com/files/4653201/download?download_frd=1&verifier=aWl6ZvPwzQ6wPLZ8H6yTh7O1lJ4Dq9ryBWReSW3M'
+    input_default = 'var'
+    txt_default = 'Please, provide a text to be analyzed'
+    Top_default = 10
+    Option_default = "Top_descriptor"
+
+    page = request.args.get('url', default= address_default,type=str)
+    type = request.args.get('type', default =input_default , type=str)
+    input_txt = request.args.get('type', default =txt_default , type=str)
+    Option = request.args.get('feature', default=Option_default, type=str)
+    Top_N = request.args.get('Occurrences', default=Top_default, type=int)
+
+    if type == 'url':
+        input_txt = getfile(page)
+
+    if Option == "Top_descriptor":
+        a= topdescriptions(input_txt,Top_N)
+        print("result", a)
+        return("OK")
+    else:
+        return("Nothing to show")
+    # we have to look on how to send back the results, converting it to xml or json
+
 
 
     
