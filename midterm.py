3e7af7555ad06c966bbc63865da1fa389d02e3e8
Brajam <47007305+Brajam@users.noreply.github.com>
2019-04-17 14:05:38 -0500

    New features + edits
    Added:
    - Global variables
    - Identify language
    - Validations wtih Gibberish based on ratio, limited text size
    - Record error with input information
    
    Modified:
    - Topdescritions to send back the descriptors ordered desc
    - format result to json

--- midterm.py
+++ midterm.py
@@ -11,14 +11,23 @@ from requests import head
 from textblob import TextBlob, Word, Blobber
 from textblob.classifiers import NaiveBayesClassifier
 from textblob.taggers import NLTKTagger
+from textblob.wordnet import VERB
+
+
 #Pandas for operate with Dataframes
 import pandas as pd
+import datetime
 
 
 # Import flask
 from flask import Flask
 from flask import jsonify
 from flask import request
+
+# Global variables
+Gib_factor = 0.1
+max_txt = 1000
+
 
 # First function, to get the files from the web
 def getfile(Input:"String list or url with the file"):
@@ -34,7 +43,7 @@ def getfile(Input:"String list or url with the file"):
 
 ## I used this function on one of the prior assigments, it gaves the top 10 descriptors, so we can keep it and
 ## adjusted as needed
-def topdescriptions(File_name:"Name of the file with the characters",Top_N:"Number of Top descriptors to be retrieved"):
+def topdescriptions(File_name:"Name of the file with the characters"):
     # Function to identify most common descriptors
     # As input we'll include the list of characters cleaned
     tb_text = TextBlob(File_name)
@@ -45,12 +54,63 @@ def topdescriptions(File_name:"Name of the file with the characters",Top_N:"Numb
         if pos[0:2] =="JJ":
             adj_list.append(word)
     # Creating a Dataframe indicating how many times the adjetive is on the list
-    count_dataframe = pd.Series(adj_list).value_counts()
-    result_desc = count_dataframe
-    return(result_desc)
+    adj_list_df = pd.DataFrame({'Descriptor':adj_list})
+    count_desc = adj_list_df['Descriptor'].value_counts()
+    count_df = count_desc.rename_axis('Descriptor').reset_index(name='Occurences')
+    print(count_df)
+    return(count_df)
 
 
+def identify_language(txt_input:"text to identify language"):
+    tb_text = TextBlob(txt_input)
+    lng_txt = tb_text.detect_language()
+    print(lng_txt)
 
+    return("OK","")
+
+def validations(txt_input:"Text to be validated",Option:"Option to be applied"):
+    global Gib_factor
+    global max_txt
+    if len(txt_input) > max_txt:
+        return("NO OK","Text too big. The total number of admited characters is "+str(max_txt))
+    if txt_input =="":
+        return("NO OK","Input empty")
+    else:
+        tb_text = TextBlob(txt_input)
+        lng_txt = tb_text.detect_language()
+        print(lng_txt)
+        if lng_txt == 'en':
+            err_word = 0
+            print(len(tb_text.tags))
+            for word, pos in tb_text.tags:
+                print(word,pos)
+                ret_word = word.synsets
+                if ret_word ==[]:
+                    err_word = err_word +1
+                else:
+                    print(ret_word)
+            if err_word/len(tb_text.tags)>Gib_factor:
+                return ("NO OK","Posible Gibberish")
+        elif lng_txt != "en" and Option != "translate" and Option != "identify":
+            return ("NO OK","For other languages only options are translate/identify language")
+        elif lng_txt == 'und':
+            return ("NO OK", "Language not supperted/not possible to identify")
+    return("OK","")
+
+
+    
+def print_error(Option,input_txt,type_input,page,error):
+    currentDT = datetime.datetime.now()
+    print("********************************************************ERROR********************************************************")
+    print("********************************************************ERROR********************************************************")
+    print("**  Error found while processing file, at:",currentDT)
+    print("**  The error found was:",error)
+    print("**  The first 10 characters of the text is",input_txt[1:10])
+    print("**  The option used:",Option)
+    print("**  The URL provided:",page)
+    print("********************************************************ERROR********************************************************")
+    print("********************************************************ERROR********************************************************")
+    return(None)
 
 # definition of the REST API
 app = Flask(__name__)
@@ -61,29 +121,36 @@ app = Flask(__name__)
 
 
 def main_process():
+    
     # defaults
     address_default = 'https://sit.instructure.com/files/4653201/download?download_frd=1&verifier=aWl6ZvPwzQ6wPLZ8H6yTh7O1lJ4Dq9ryBWReSW3M'
     input_default = 'var'
-    txt_default = 'Please, provide a text to be analyzed'
-    Top_default = 10
+    txt_default = ''
     Option_default = "Top_descriptor"
 
     page = request.args.get('url', default= address_default,type=str)
-    type = request.args.get('type', default =input_default , type=str)
-    input_txt = request.args.get('type', default =txt_default , type=str)
+    type_input = request.args.get('type', default =input_default , type=str)
+    input_txt = request.args.get('text', default =txt_default , type=str)
     Option = request.args.get('feature', default=Option_default, type=str)
-    Top_N = request.args.get('Occurrences', default=Top_default, type=int)
+    
 
     if type == 'url':
         input_txt = getfile(page)
+        
+    result,error = validations(input_txt,Option)
+
+    if result != "OK":
+        print_error(Option,input_txt,type_input,page,error)
+        return error
 
     if Option == "Top_descriptor":
-        a= topdescriptions(input_txt,Top_N)
-        print("result", a)
-        return("OK")
+        desciption_result = topdescriptions(input_txt)
+        result_json = desciption_result.to_json(orient='split')
+        print("result", result_json)
+        return("OK", result_json)
     else:
         return("Nothing to show")
-    # we have to look on how to send back the results, converting it to xml or json
+    #Results to be sent back in json
 
 
 
